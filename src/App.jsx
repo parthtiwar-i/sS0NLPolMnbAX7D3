0a2f0e624e985ebb9ee8cd6e1bf02471bd070610
Parth Tiwari <tiwariparth02@gmail.com>
2024-01-01 13:11:20 +0530

    setting up App.js with react-router-dom to make routes Home and BMICalculator with components Home and BMICalculate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-
-    </div>
+    <Router basename='/sS0NLPolMnbAX7D3/'>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/bmi' element={<BMICalculator/>} ></Route>
+      </Routes>
+    </Router>
   )
 }
 
